593eea92461add16757b9a30cc36b769052f4147
Pratik-22 <pratikgawari0000@gmail.com>
2023-04-10 21:33:02 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { v4 as uuidv4 } from 'uuid';
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem('ITEMS')
+    if (localValue == null) { return [] }
+    // Why JSON.parse
+    return JSON.parse(localValue)
+  })
+  useEffect(() => {
+    // Why JSON.stringify
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
 
   function addTodo(title) {
     setTodos((currentTodos) => {
